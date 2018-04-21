d8033291ec15c0a9dcda68446be2382965637d55
Pernas <bslucena@gmail.com>
2018-04-21 21:56:50 +0200

    Added movement and camera

--- PestControl/GameScene.swift
+++ PestControl/GameScene.swift
@@ -15,10 +15,32 @@ class GameScene: SKScene {
     
     override func didMove(to view: SKView) {
         addChild(player)
+        setupCamera()
     }
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         background = childNode(withName: "background") as! SKTileMapNode
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        guard let touch = touches.first else { return }
+        
+        // This method takes the touch location and sends it to the move(target:) method of Player.
+        player.move(target: touch.location(in: self))
+    }
+    
+    func setupCamera() {
+        
+        // Check that the scene has a camera assigned to it.
+        guard let camera = camera else { return }
+        
+        // Create a constraint to keep zero distance to the player.
+        let zeroDistance = SKRange(constantValue: 0)
+        let playerConstraint = SKConstraint.distance(zeroDistance, to: player)
+        
+        // Assign that constraint to the camera, so the camera will always follow the player and keep him centered onscreen.
+        camera.constraints = [playerConstraint]
     }
 }
